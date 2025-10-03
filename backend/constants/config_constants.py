06a9fcbbec72be60ef94a1f4e4573cde42cc9cd7
Divya Nayan <divyanayan88@gmail.com>
2025-10-03 16:07:42 +0530

    Remove hardcoded database driver - use environment variables only

--- backend/constants/config_constants.py
+++ backend/constants/config_constants.py
@@ -3,7 +3,6 @@ Configuration constants
 """
 
 # Database
-DEFAULT_DB_DRIVER = '{ODBC Driver 18 for SQL Server}'
 DATABASE_CONNECTION_TIMEOUT = 30
 DATABASE_RETRY_ATTEMPTS = 3
 DATABASE_RETRY_DELAY = 2
